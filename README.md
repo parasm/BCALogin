90d932c73d72066a0af8c510563a31f244686d03
Paras Modi <parasm346@gmail.com>
2013-11-26 22:22:28 -0500

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 BCALogin
 ========
+<h1>Automatic BCA Wifi Sign In</h1>
+<p>Simply enter your username and password the first time you open the extenstion</p>
+<br>
+<p>Then from then on it will remember you and jsut click on the extension to be signed in</p>
+<br>
+<h3>Updates to Come:</h3>
+<ul>
+	<li>Automatic Sign in every 60 minutes</li>
+</ul>
---
+++ login.js
@@ -0,0 +1,91 @@
+$(function() {
+    chrome.cookies.get({ url: 'https://www.google.com/', name: 'sign_in' },
+    function (cookie) {
+        if (cookie) {
+          cookieFound(cookie.value);
+        }
+        else{
+            cookieNotFound();
+        }
+    });
+    function cookieFound(cookie){
+        var user_pass = cookie.split("-");
+        var username = user_pass[0];
+        var password = user_pass[1];
+        var url = "https://ccahack.bergen.org/auth/perfigo_weblogin.jsp?cm=ws32vklm&uri=https%3A%2F%2Fccahack.bergen.org%2F";
+        $.ajax({
+            type : 'GET',
+            url: "http://localhost:8000/stuff.html",
+            success: function(data){
+                var form = $(data);
+                var ip = form.find("input[name='userip']").val();
+                login(ip);
+            }
+        });
+        function login(ip){
+            $.ajax({
+                type : 'POST',
+                url : 'https://ccahack.bergen.org/auth/perfigo_validate.jsp',
+                data : {
+                    reqFrom: 'perfigo_simple_login.jsp',
+                    uri: 'https://ccahack.bergen.org/',
+                    cm: 'ws32vklm',
+                    userip: ip,
+                    os: 'MAC_OSX',
+                    index: '4',
+                    username: username,
+                    password: password,
+                    provider: 'BCA',
+                    login_submt: 'Continue'
+                },
+                success : worked
+            });
+        }
+    }
+    function cookieNotFound(){
+        $('body').append('<form method="POST" id="form"><label>Enter username<input id="username"></label><br>'+
+            '<label>Enter password <input id="password" type="password">  </label> '+
+            '<br><button type="submit" id="button">Sign In</button>'+
+            '</form><button id="clear">Clear Saved Login</button>');
+        $('#form').submit(function (event) {
+            event.preventDefault();
+            var username = $('#username').val();
+            var password = $('#password').val();
+            var url = "https://ccahack.bergen.org/auth/perfigo_weblogin.jsp?cm=ws32vklm&uri=https%3A%2F%2Fccahack.bergen.org%2F";
+            $.ajax({
+                type : 'GET',
+                url: "http://localhost:8000/stuff.html",
+                success: function(data){
+                    var form = $(data);
+                    var ip = form.find("input[name='userip']").val();
+                    login(ip);
+                }
+            });
+            function login(ip){
+                $.ajax({
+                    type : 'POST',
+                    url : 'https://ccahack.bergen.org/auth/perfigo_validate.jsp',
+                    data : {
+                        reqFrom: 'perfigo_simple_login.jsp',
+                        uri: 'https://ccahack.bergen.org/',
+                        cm: 'ws32vklm',
+                        userip: ip,
+                        os: 'MAC_OSX',
+                        index: '4',
+                        username: username,
+                        password: password,
+                        provider: 'BCA',
+                        login_submt: 'Continue'
+                    },
+                    success : worked
+                });
+            }
+            chrome.cookies.set({ url: "https://www.google.com/", name: "sign_in", value: username+"-"+password });
+        });
+    }
+    function worked(){
+        $('#form').remove();
+        $('#clear').remove();
+        $('body').append('<h1>Successfully Logged In!</h1>');
+    }
+});
